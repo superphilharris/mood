99bc14d82298fe938674a7ca89a982b7e87e53f3
Philip Harris <philip@the-harrises.com>
2018-02-23 19:06:26 +1300

    init

---
+++ src/main/java/co/logbook/mood/MoodApplication.java
@@ -0,0 +1,12 @@
+package co.logbook.mood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodApplication.class, args);
+	}
+}
---
+++ src/test/java/co/logbook/mood/MoodApplicationTests.java
@@ -0,0 +1,16 @@
+package co.logbook.mood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
