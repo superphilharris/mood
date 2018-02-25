ef109f90fbf37fc88b3baa3f2cfc9c8b29b72a14
superphilharris <philip@the-harrises.com>
2018-02-25 21:55:02 +1300

    Expire cookie at midnight

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
-var app = angular.module('moodApp', []);
+var app = angular.module('moodApp', ['ngCookies']);
 
-app.controller('moodCtrl', function($scope, $http) {
+app.controller('moodCtrl', function($scope, $http, $cookies) {
     $scope.new_mood = {
         comment: "",
         feeling: "BIT_MEH"
@@ -10,7 +10,17 @@ app.controller('moodCtrl', function($scope, $http) {
         moods: [],
         closestFeeling: "BIT_MEH"
     }
-    $scope.current_view = "TRY_TOMORROW"// "ADD_NEW_MOOD";
+    $scope.current_view = ($cookies.get('LAST_FEELING') == null)? "ADD_NEW_MOOD" : "TRY_TOMORROW";
+
+    var getTonight = function() {
+        var tonight = new Date();
+        tonight.setDate(tonight.getDate() + 1)
+        tonight.setHours(0,0,0,0)
+        return tonight;
+    }
+    var setFeelingCookie = function(feeling){
+        $cookies.put('LAST_FEELING', feeling, { expires: getTonight() })
+    }
 
     $scope.showAverage = function() {
         $scope.current_view = "AVERAGE_MOOD";
@@ -24,6 +34,7 @@ app.controller('moodCtrl', function($scope, $http) {
         $http.post("mood/add", $scope.new_mood).error(function(data){
             console.log("ERROR: " + JSON.stringify(data));
         }).success(function(data){
+            setFeelingCookie($scope.new_mood.feeling)
             $scope.showAverage();
         });
     }
